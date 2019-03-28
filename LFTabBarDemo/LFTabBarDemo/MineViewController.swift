37617a870d74b041efc1b098416635643657b03a
吴林丰 <wulinfeng@chehang168.com>
2019-03-28 11:39:59 +0800

    测试

--- LFTabBarDemo/LFTabBarDemo/MineViewController.swift
+++ LFTabBarDemo/LFTabBarDemo/MineViewController.swift
@@ -14,6 +14,7 @@ class MineViewController: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.gray
         self.navigationItem.title = "个人中心"
+        NSLog("大幅度发", "ddd");
         // Do any additional setup after loading the view.
     }
 
